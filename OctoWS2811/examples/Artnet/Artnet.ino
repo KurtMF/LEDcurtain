b5cdabdd6e989b1bcafbf77ed419428da88ce054
KurtMF <amspay@nc.rr.com>
2025-02-15 15:16:05 -0500

    Update Artnet.ino Again
    replaced for loop with full buffer memcpy() call

--- OctoWS2811/examples/Artnet/Artnet.ino
+++ OctoWS2811/examples/Artnet/Artnet.ino
@@ -82,10 +82,7 @@ void onDmxFrame(uint16_t universe, uint16_t length, uint8_t sequence, uint8_t* d
   }      
 
   // send to leds
-  for (int i = 0; i < ledsPerStrip * numStrips; i++)
-  {
-    memcpy(&drawingMemory[i * 3], &channelBuffer[i * 3], 3);
-  } 
+  memcpy(&drawingMemory[0], &channelBuffer[0], ledsPerStrip * numStrips * 3);
   
   if (sendFrame)
   {
